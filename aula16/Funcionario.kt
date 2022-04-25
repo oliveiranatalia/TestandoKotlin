46dbacab3d3f8f2155d749f3d09b97c3b5e38aa8
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-04-25 17:35:44 -0300

    Subir exercicio

---
+++ aula16/Funcionario.kt
@@ -0,0 +1,20 @@
+package classesobjetos.aula16
+
+class Funcionario {
+    var nome: String = ""
+    var sobrenome: String = ""
+    var horasTrabalhadas = 0
+    var valorHora = 0.0
+
+    fun nomeCompleto(){
+        println("Nome: $nome $sobrenome")
+    }
+    fun calcularSalario(){
+        val resultado = horasTrabalhadas * valorHora
+        print("O valor total é de R$ $resultado.")
+    }
+    fun incrementarHoras(novoValor: Double): Double {
+        valorHora += novoValor
+        return valorHora
+    }
+}
