e6fa951d92748d25382f1460f0753204e2aa4f81
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-04-25 23:31:44 -0300

    subir ex

---
+++ aula13/03gostoAlgoritmo.kt
@@ -0,0 +1,8 @@
+//Exiba 20 vezes a mensagem “Eu gosto de estudar Algoritmos!”. Utilize While
+fun main() {
+    var frase = 0
+    while (frase <= 20) {
+        println("Eu gosto de estudar Algoritmos!")
+        frase++
+    }
+}
