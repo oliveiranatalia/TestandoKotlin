f5e884f6e7d2c4f10fd7a7a1fbf85af687a7a67b
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-04-27 12:13:14 -0300

    Enxugar script

--- aula16/aula16/livros/Livros.kt
+++ aula16/aula16/livros/Livros.kt
@@ -1,10 +1,6 @@
 package classesobjetos.aula16.livros
 
-class Livros {
-
-    var titulo = ""
-    var qtdPaginas= 0
-    var paginasLidas = 0
+class Livros (var titulo: String, var qtdPaginas: Int, var paginasLidas:Int)  {
     fun verificarProgresso(){
         val porcentagem = paginasLidas * 100 / qtdPaginas
         println("Você leu $porcentagem% do livro!")}
